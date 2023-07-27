ba14f3119d2daf1b43cc2825eb40e88b2e92431c
Oleja Iv <oleg1242@gmail.com>
2023-07-27 15:40:28 +0300

    update test_capitalize module

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,3 +1,9 @@
 from capitalize import capitalize
 
-if capitalize
+if capitalize('hello!') != 'Hello!':
+    raise Exeption("Doesn't work!")
+
+if capitalize('') != '':
+    raise Exception("Not work with empty string!")
+
+print('All works correctly!')
